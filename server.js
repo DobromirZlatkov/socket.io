a455536c5ef3ffeb1b89078ee508bc138efbe2ac
Dobromir Zlatkov <dz@Petars-Mac-mini.local>
2015-11-03 17:41:34 +0200

    fixed heroku stuff

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ var express = require('express'),
     port = process.env.PORT || 4000,
     server = http.createServer(app).listen(port),
     io = require('socket.io').listen(server),
-    redis = require('socket.io/node_modules/redis');
-    // sub = redis.createClient(),
-    // sub.subscribe('chat');
+    url = require('url'),
+    redisURL = url.parse(process.env.REDISCLOUD_URL),
+    redis = require('socket.io/node_modules/redis'),
+    sub = redis.createClient(redisURL.port, redisURL.hostname, {return_buffers: true});
+    sub.subscribe('chat');
 
-
+//port_arg, host_arg, options
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
@@ -16,7 +18,11 @@ app.get('/', function (req, res) {
 
 io.sockets.on('connection', function (socket) {
 
-    // sub.on('message', function(channel, message){
-    //     socket.send(message);
-    // });
+    sub.on('message', function(channel, message){
+        socket.send(message);
+    });
+
+    socket.on("close", function() {
+        console.log("websocket connection close");
+    });
 });
