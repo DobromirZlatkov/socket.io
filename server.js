6a39dd4d1483cc59c6509218d6ddba88fd3595aa
Dobromir Zlatkov <dz@Petars-Mac-mini.local>
2015-11-03 16:11:27 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+    app = express(),
+    http = require('http'),
+    port = process.env.PORT || 4000,
+    server = http.createServer(app).listen(port),
+    io = require('socket.io').listen(server),
+    redis = require('socket.io/node_modules/redis');
+    sub = redis.createClient(),
+    sub.subscribe('chat');
+
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+io.sockets.on('connection', function (socket) {
+
+    sub.on('message', function(channel, message){
+        socket.send(message);
+    });
+});
