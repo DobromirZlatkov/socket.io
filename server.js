c5f40490d89eb4c888b14beada3431df58dc71f4
Dobromir Zlatkov <dz@Petars-Mac-mini.local>
2015-11-03 18:35:10 +0200

    cors enable

--- server.js
+++ server.js
@@ -11,6 +11,23 @@ var express = require('express'),
     sub = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
     sub.subscribe('chat');
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.configure(function () {
+    app.use(allowCrossDomain);
+});
 //port_arg, host_arg, options
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
