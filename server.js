fa1bd0a2ccd6fd2131db517c50e214b2627897ec
Dobromir Zlatkov <dz@Petars-Mac-mini.local>
2016-04-11 18:04:06 +0300

    Allow all cors.

--- server.js
+++ server.js
@@ -22,8 +22,10 @@ var redis = require('redis'),
 
 //TODO handle multiple requests that receive from redis..
 
+
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://foldedapp.herokuapp.com');
+    // res.header('Access-Control-Allow-Origin', 'http://foldedapp.herokuapp.com');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     next();
